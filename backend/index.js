5a70cd2ec95c1b4a090c6462b6f6aed1b61f3717
Makaosva <maka.osses@gmail.com>
2024-12-14 17:48:51 -0300

    feat/cambio en cors

--- backend/index.js
+++ backend/index.js
@@ -12,13 +12,13 @@ const app = express();
 const usersRoutes = require("./routes/usersRoutes");
 
 // Configuración de CORS para permitir solicitudes con credenciales
-const corsOptions = {
-  // origin: "https://hito-4-render.onrender.com/",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-  exposedHeaders: ["Authorization"], // Permitir enviar cookies o encabezados de autorización
-};
-app.use(cors(corsOptions)); // se permite cors para todas las rutas
+// const corsOptions = {
+//   // origin: "https://hito-4-render.onrender.com/",
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true,
+//   exposedHeaders: ["Authorization"], // Permitir enviar cookies o encabezados de autorización
+// };
+app.use(cors()); // se permite cors para todas las rutas
 /* parsear el cuerpo de la consulta */
 app.use(express.json());
 //app.use("/publicaciones", usersRoutes);
