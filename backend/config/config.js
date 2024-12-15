30eeab1f43674914e941c0e28041f3e7c8a1cd18
Makaosva <maka.osses@gmail.com>
2024-12-15 04:57:15 -0300

    feat/cambios CAr

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,16 +3,20 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.PGHOST || "localhost",
+  /* host: process.env.PGHOST || "localhost",
   user: process.env.PGUSER || "postgres",
-  password: process.env.PGPASSWORD || "mo76492250",
+  password: process.env.PGPASSWORD || "sql7777",
   database: process.env.PGDATABASE || "market",
   port: process.env.PGPORT || 5432,
-  allowExitOnIdle: true,
-});
-// Manejo de errores de la base de datos
-pool.on("error", (err) => {
-  console.error("Error en la conexión a la base de datos:", err);
+  allowExitOnIdle: true, */
+
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+
 });
 
-module.exports = pool;
+
+module.exports =  pool ;
