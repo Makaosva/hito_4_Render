f4ff3592fb3814bafc589044ed455c69d4244b24
Makaosva <maka.osses@gmail.com>
2024-12-15 00:23:14 -0300

    feat/arreglos segun readme

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "../backend/public", // Construir el frontend en la carpeta del backend
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000", // Proxy para evitar problemas de CORS
+    },
+  },
+});
